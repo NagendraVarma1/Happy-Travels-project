96e43709bc987141c19c5171c9c7f5b729f291fd
NagendraVarma1 <nagendravarmal512@gmail.com>
2026-02-21 11:24:48 +0530

    quick access buttons added

---
+++ src/Components/QuickAccessBtn/QuickAccessBtn.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import classes from './QuickAccessBtn.module.css'
+import { BsWhatsapp } from "react-icons/bs";
+import { FaPhoneAlt } from "react-icons/fa";
+import { ImCancelCircle } from "react-icons/im";
+import { MdOutlineMessage } from "react-icons/md";
+
+const QuickAccessBtn = () => {
+    const [showAllIcons, setShowAllIcons] = useState(false)
+     const handleClick = () => {
+    const phoneNumber = "917795375321"; // Use country code without +
+    const url = `https://wa.me/${phoneNumber}`;
+    window.open(url, "_blank");
+  };
+    return (
+        <div className={classes.quickAccessBtnDiv}>
+            <BsWhatsapp onClick={handleClick} className={showAllIcons? classes.whatsAppBtn : classes.whatsAppBtnHidden}/>
+            <a href='tel:7795375321'><FaPhoneAlt className={showAllIcons? classes.phoneBtn : classes.phoneBtnHidden} /></a>
+            <ImCancelCircle onClick={() => setShowAllIcons(false)} className={showAllIcons? classes.cancelBtn : classes.cancelBtnHidden} />
+            <MdOutlineMessage onClick={() => setShowAllIcons(true)} className={showAllIcons? classes.messageBtnHidden : classes.messageBtn} />
+        </div>
+    )
+}
+
+export default QuickAccessBtn
--- src/Components/RootLayout/RootLayout.js
+++ src/Components/RootLayout/RootLayout.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import QuickAccessBtn from "../QuickAccessBtn/QuickAccessBtn";
 
 
 const RootLayout = () => {
@@ -8,6 +9,7 @@ const RootLayout = () => {
     <>
       <Header />
       <Outlet />
+      <QuickAccessBtn />
       <Footer />
     </>
   );
